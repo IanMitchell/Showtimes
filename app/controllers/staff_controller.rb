33a6883a2938676c43ec934f22d6af864a838d6e
Ian Mitchell <ianm@zillow.com>
2015-12-21 18:08:46 -0800

    API Updates
    Time for Belfiore work!

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,11 +1,20 @@
 class StaffController < ApplicationController
   def update
-    @staff = Show.find_by(name: params[:name])
-              .fansubs.where(group: Group.find_by(irc: params[:irc])).first
-              .current_release.staff
-              .where(user: User.find_by(name: params[:username])).first
+    # TODO: Update passphrase and move to ENV variable
+    if params[:auth].eql? 'secretpassword'
+      @staff = Show.find_by(name: params[:name])
+                .fansubs.where(group: Group.find_by(irc: params[:irc])).first
+                .current_release.staff
+                .where(user: User.find_by(name: params[:username])).first
 
-    @staff.update_attribute :status, params[:status]
-    # TODO: handle errors / return a value
+      if [:pending, :finished].include? params[:status].to_sym
+        @staff.update_attribute :status, params[:status].to_sym
+        render json: { message: "Updated #{params[:name]}" }, status: 200
+      else
+        render json: { message: 'Invalid; Use `pending` or `finished`' }, status: 400
+      end
+    else
+      render json: { message: 'Unauthorized Request' }, status: 401
+    end
   end
 end
