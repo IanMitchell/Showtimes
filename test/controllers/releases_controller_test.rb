f0ada72e699efe26391e9f3916ac4ef54cd19241
Ian Mitchell <ianm@zillow.com>
2016-02-08 15:10:48 -0800

    Partial Release Testing

--- test/controllers/releases_controller_test.rb
+++ test/controllers/releases_controller_test.rb
@@ -1,4 +1,71 @@
 require 'test_helper'
 
 class ReleasesControllerTest < ActionController::TestCase
+  test 'should require authentication' do
+    put :update, {
+      username: 'Desch',
+      auth: 'wrongpassword',
+      irc: '#gjm',
+      name: 'aoty',
+      format: :json
+    }
+    assert_response 401
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('unauthorized'), 'Incorrect error message'
+  end
+
+  test 'should fail with incorrect channel' do
+    put :update, {
+      username: 'Desch',
+      auth: 'secretpassword',
+      irc: '#gjm',
+      name: 'aoty',
+      format: :json
+    }
+    assert_response 400
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('irc'), 'Incorrect error message'
+  end
+
+  test 'should fail with non-staff channel' do
+    put :update, {
+      username: 'Desch',
+      auth: 'secretpassword',
+      irc: '#cartel',
+      name: 'aoty',
+      format: :json
+    }
+    assert_response 400
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('irc'), 'Incorrect error message'
+  end
+
+  test 'should fail with incorrect show' do
+    put :update, {
+      username: 'Desch',
+      auth: 'secretpassword',
+      irc: '#cartel-staff',
+      name: 'aoty2',
+      format: :json
+    }
+    assert_response 400
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('show'), 'Incorrect error message'
+  end
+
+  test 'should fail with incorrect fansub' do
+  end
+
+  test 'should fail with finished show' do
+  end
+
+  test 'should require all positions to be complete' do
+  end
+
+  test 'should succeed for correct show' do
+  end
 end
