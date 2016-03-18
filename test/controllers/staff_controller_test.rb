5a6f20ac7e2b145266c4b3dcc3ea63f2d4b2759a
Ian Mitchell <ianm@zillow.com>
2016-03-18 15:40:23 -0700

    Tests Implemented

--- test/controllers/staff_controller_test.rb
+++ test/controllers/staff_controller_test.rb
@@ -1,25 +1,67 @@
 require 'test_helper'
 
 class StaffControllerTest < ActionController::TestCase
-  test 'should not require position when user has one position' do
+  test 'should restrict command to authorized requests' do
+    put :update, {
+      auth: 'lolno',
+      irc: '#cartel',
+      username: 'Desch',
+      name: 'aoty',
+      status: 'true',
+      format: :json
+    }
+
+    assert_response 401
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('unauthorized'), 'Incorrect error message'
   end
 
-  test 'should require position when user has multiple positions' do
+  test 'should not allow non-staff channels' do
+    put :update, {
+      auth: ENV['AUTH'],
+      irc: '#cartel',
+      username: 'Desch',
+      name: 'aoty',
+      status: 'true',
+      format: :json
+    }
+
+    assert_response 400
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('channel'), 'Incorrect error message'
   end
 
-  test 'should only allow staff channels' do
+  test 'should not allow non-staff nicks' do
+    put :update, {
+      auth: ENV['AUTH'],
+      irc: '#cartel-staff',
+      username: 'Belfiore',
+      name: 'aoty',
+      status: 'true',
+      format: :json
+    }
+
+    assert_response 400
+
+    body = JSON.parse(response.body)
+    assert body['message'].downcase.include?('nick'), 'Incorrect error message'
   end
 
-  test 'should only allow staff nicks' do
-  end
-
-  test 'should only allow fansubbed shows' do
+  test 'should not allow non-fansubbed shows' do
   end
 
   test 'should not allow non-staff to update show' do
   end
 
   test 'should not allow invalid positions' do
+  end
+
+  test 'should not require position when user has one position' do
+  end
+
+  test 'should require position when user has multiple positions' do
   end
 
   test 'should not allow incorrect staff position' do
