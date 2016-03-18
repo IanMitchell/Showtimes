45a61468fde7a76cb073a06b2eb12fd90d6ce557
Ian Mitchell <ianm@zillow.com>
2016-03-18 15:20:13 -0700

    Staff Refactor and Test Stubs

--- test/controllers/staff_controller_test.rb
+++ test/controllers/staff_controller_test.rb
@@ -1,4 +1,33 @@
 require 'test_helper'
 
 class StaffControllerTest < ActionController::TestCase
+  test 'should not require position when user has one position' do
+  end
+
+  test 'should require position when user has multiple positions' do
+  end
+
+  test 'should only allow staff channels' do
+  end
+
+  test 'should only allow staff nicks' do
+  end
+
+  test 'should only allow fansubbed shows' do
+  end
+
+  test 'should not allow non-staff to update show' do
+  end
+
+  test 'should not allow invalid positions' do
+  end
+
+  test 'should not allow incorrect staff position' do
+  end
+
+  test 'should allow founders to update any position for any show' do
+  end
+
+  test 'should correctly update show' do
+  end
 end
