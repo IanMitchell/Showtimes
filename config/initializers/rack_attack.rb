884be98e6e8c33e3676f17fce94110b988b77f35
Ian Mitchell <ianm@zillowgroup.com>
2016-10-05 11:11:55 -0700

    Additional Config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,16 +1,6 @@
 class Rack::Attack
-  # After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
-  Rack::Attack.blocklist('fail2ban pentesters') do |req|
-    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
-    # so the request is blocked
-    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}",
-                                  maxretry: 3,
-                                  findtime: 10.minutes,
-                                  bantime: 5.minutes) do
-      # The count for the IP is incremented if the return value is truthy
-      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-      req.path.include?('/users/sign_in')
-    end
+  Rack::Attack.blocklist('block 179.216.235.85') do |req|
+    req.ip.eql? '179.216.235.85'
   end
 
   # Throttle requests to 5 requests per second per ip
