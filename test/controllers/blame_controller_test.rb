c6fd9eaccd5f59b57f44a0b4be4dd86e2f35818d
Ian Mitchell <ian.mitchel1@live.com>
2020-07-26 14:38:27 -0700

    Work on Prioritizing Blame

--- test/controllers/blame_controller_test.rb
+++ test/controllers/blame_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class BlameControllerTest < ActionController::TestCase
   test 'should succeed for subbed show' do
-    get :show, params: { channel: '1', show: 'aoty', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'aoty', format: :json }
     assert_response :success
 
     body = JSON.parse(response.body)
@@ -22,7 +22,7 @@ class BlameControllerTest < ActionController::TestCase
   end
 
   test 'should fail with incorrect fansub' do
-    get :show, params: { channel: '1', show: 'shomin', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'shomin', format: :json }
     assert_response 400
 
     body = JSON.parse(response.body)
@@ -31,7 +31,7 @@ class BlameControllerTest < ActionController::TestCase
   end
 
   test 'should fail with completed fansub' do
-    get :show, params: { channel: '1', show: 'kimi', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'kimi', format: :json }
     assert_response 400
 
     body = JSON.parse(response.body)
@@ -40,17 +40,17 @@ class BlameControllerTest < ActionController::TestCase
   end
 
   test 'should ignore multiple matches for irrelevant shows' do
-    get :show, params: { channel: '1', show: 'desch', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'desch', format: :json }
     assert_response 200
   end
 
   test 'should respond to term' do
-    get :show, params: { channel: '1', show: 'aoty', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'aoty', format: :json }
     assert_response 200
   end
 
   test 'should handle multiple show matches' do
-    get :show, params: { channel: '1', show: 'shigatsu', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'shigatsu', format: :json }
     assert_response 400
 
     body = JSON.parse(response.body)
@@ -69,10 +69,18 @@ class BlameControllerTest < ActionController::TestCase
   end
 
   test 'should find show based on partial encoded name' do
-    get :show, params: { channel: '1', show: 'Sekai%20Ni', format: :json }
+    get :show, params: { channel: 'cartel_discord', show: 'Sekai%20Ni', format: :json }
     assert_response :success
 
     body = JSON.parse(response.body)
     assert body['name'], 'Name not in response'
+  end
+
+  test 'should prioritize incomplete fansubs' do
+    get :show, params: { channel: 'cartel_discord', show: 'desch', format: :json }
+    assert_response 200
+
+    body = JSON.parse(response.body)
+    assert_equal body['name'], 'name', 'Name not in response'
   end
 end
