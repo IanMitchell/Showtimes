f4d54b6f80ab8b094e9d48d61462ad04ffff1fa1
Ian Mitchell <ian.mitchel1@live.com>
2020-07-26 14:51:32 -0700

    Fix Deprecations

--- app/controllers/blame_controller.rb
+++ app/controllers/blame_controller.rb
@@ -3,7 +3,7 @@ class BlameController < ApplicationController
 
   def show
     @group = Group.find_by_discord(params[:channel])
-    @fansub = @group.find_fansub_for_show_prioritized_fuzzy(URI.decode(params[:show]))
+    @fansub = @group.find_fansub_for_show_prioritized_fuzzy(URI.decode_www_form_component(params[:show]))
     @release = @fansub.current_release
   end
 end
